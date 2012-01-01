06e5bc0e4513be2d49e10dc9feb93850a536b1e3
Glenn Gillen <me@glenngillen.com>
2012-01-01 13:58:26 +1100

    MOAR manifests.

---
+++ templates/social/manifest.rb
@@ -0,0 +1,11 @@
+description "Social Icons"
+
+image 'responsive-boilerplate/auth-icons.png'
+
+help %Q{
+If you need help, ask Glenn Gillen (@glenngillen).
+}
+
+welcome_message %Q{
+Congrats! You have installed the social icons!
+}
