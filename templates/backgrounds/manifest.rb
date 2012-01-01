bbc3ec0f50a66ea12ef47a69c0a8b0ebe74e601b
Glenn Gillen <me@glenngillen.com>
2012-01-01 13:51:36 +1100

    More tweaking of manifests.

---
+++ templates/backgrounds/manifest.rb
@@ -0,0 +1,12 @@
+description "Background Textures"
+
+image 'responsive-boilerplate/bright-squares.png'
+image 'responsive-boilerplate/noise.png'
+
+help %Q{
+If you need help, ask Glenn Gillen (@glenngillen).
+}
+
+welcome_message %Q{
+Congrats! You have installed the background textures!
+}
